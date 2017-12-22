5228d1ca907a0c806a95a01f745e1bceec44a68f
dabaimao <shawnping@outlook.com>
2017-12-22 18:55:59 +0800

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ball; //BowlingBall
+    private float maxWidth;
+    private float time = 2;
+    private GameObject newball;
+	// Use this for initialization
+	void Start ()
+    {
+        //将屏幕宽度转换成世界坐标
+        Vector3 screenPos = new Vector3(Screen.width, 0, 0);
+        Vector3 moveWidth = Camera.main.ScreenToViewportPoint(screenPos);
+        //获取保留球自身宽度
+        float ballWidth = ball.GetComponent<Renderer>().bounds.extents.x;
+        //计算保龄球实例化的位置的宽度
+        maxWidth = moveWidth.x - ballWidth;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        time -= Time.deltaTime;
+        if(time<0)
+        {
+            //产生一个随机数，代表实例化下一个保龄球所需时间
+            time = Random.Range(1.5f, 2.0f);
+            /*在保龄球实例化位置宽度内产生一个随机数，来控制实例化的保龄球的位置*/
+            float posX = Random.Range(-maxWidth, maxWidth);
+            Vector3 spawnPosition = new Vector3(posX, transform.position.y, 0);
+            //实例化保龄球
+            newball = (GameObject)Instantiate(ball, spawnPosition, Quaternion.identity);
+            Destroy(newball, 10);
+        }
+	}
+}
---
+++ HatController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatController : MonoBehaviour
+{
+    public GameObject effect;
+    private Vector3 rawPosition;
+    private Vector3 hatPosition;
+    private float maxWidth;
+    // Use this for initialization
+    void Start ()
+    {
+        Vector3 screenPos = new Vector3(Screen.width, 0, 0);
+        Vector3 moveWidth = Camera.main.ScreenToWorldPoint(screenPos);
+        //计算帽子的宽度
+        float hatWidth = GetComponent<Renderer>().bounds.extents.x;
+        //获得帽子的初始位置
+        hatPosition = transform.position;
+        //计算帽子的移动速度
+        maxWidth = moveWidth.x - hatWidth;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //将鼠标的屏幕位置转换成世界坐标
+        rawPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //设置帽子将要移动的位置，帽子移动范围控制
+        hatPosition = new Vector3(rawPosition.x, hatPosition.y, 0);
+        hatPosition.x = Mathf.Clamp(hatPosition.x, -maxWidth, maxWidth);
+        //帽子移动
+        GetComponent<Rigidbody2D>().MovePosition(hatPosition);
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject neweffect = (GameObject)Instantiate(effect, transform.position, effect.transform.rotation);
+        neweffect.transform.parent = transform;
+        Destroy(col.gameObject);
+        Destroy(neweffect, 1.0f);
+    }
+}
